00a88a39d1ce756ce41a527d8d49d860809c08c7
Roman Tarasov <rom4ncer@gmail.com>
2023-09-20 22:22:33 +0300

    init: complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.num = parseInt((this.left + this.right + 1) / 2);
+        return this.num;
     }
 
     lower() {
-
+        this.right = this.num;
     }
 
     greater() {
-
+        this.left = this.num;
     }
 }
 
